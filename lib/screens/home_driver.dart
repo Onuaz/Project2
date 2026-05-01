e0fabe7d9337d18116f70162101b0f7b11e460df
Onuaz <109168684+Onuaz@users.noreply.github.com>
2026-05-01 16:34:03 -0400

    Add placeholder home screens for each role

---
+++ lib/screens/home_driver.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HomeDriver extends StatelessWidget {
+  const HomeDriver({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: Center(child: Text('Driver Home')));
+  }
+}
